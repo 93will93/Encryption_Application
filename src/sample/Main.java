5591b98522465d3f98a8015c4f5a2889890f076a
William Becerra Gonzalez <wbecerra1@gmail.com>
2017-07-19 18:16:02 +0200

    More neat front end design-  Still no functionallity.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import  javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import  javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -26,28 +27,40 @@ public class Main extends Application {
             grid.setVgap(8);
             grid.setHgap(10);
 
-            // encryption label and text field;
-            Label plaintextLable = new Label("plaintext");
-            GridPane.setConstraints(plaintextLable, 0, 0);
-
-            TextField inputPlaintext = new TextField();
-            inputPlaintext.setPromptText("Example: Today is a sunny day");
-            GridPane.setConstraints(inputPlaintext, 1, 0);
-
             // password label and Text field.
             Label passLabel = new Label("Password");
-            GridPane.setConstraints(passLabel, 0, 1);
+            GridPane.setConstraints(passLabel, 0, 0);
 
             TextField inputPass = new TextField();
             inputPass.setPromptText("Example: xyzt1993");
-            GridPane.setConstraints(inputPass, 1,1);
+            GridPane.setConstraints(inputPass, 1,0);
+
+
+        // encryption label and text field;
+            Label plaintextLable = new Label("Text");
+            GridPane.setConstraints(plaintextLable, 0, 1);
+
+            TextArea inputPlaintext = new TextArea();
+            inputPlaintext.setPromptText("Example: Today is a sunny day");
+            GridPane.setConstraints(inputPlaintext, 1, 1);
+
 
             //Creating the encrypt  button
              Button encryptButton = new Button("Encrypt");
              GridPane.setConstraints(encryptButton, 0, 2);
 
-            grid.getChildren().addAll(plaintextLable, inputPlaintext, passLabel, inputPass, encryptButton);
-            Scene scene = new Scene(grid, 500, 500);
+
+            // encryption label and text field;
+            Label Encrypted_Lable = new Label("Text");
+            GridPane.setConstraints(Encrypted_Lable, 0, 3);
+
+            TextArea Encrypted_Text = new TextArea();
+            Encrypted_Text.setPromptText("Example: Today is a sunny day");
+            GridPane.setConstraints(Encrypted_Text, 1, 3);
+
+
+            grid.getChildren().addAll(plaintextLable, inputPlaintext, passLabel, inputPass, encryptButton, Encrypted_Text, Encrypted_Lable);
+            Scene scene = new Scene(grid, 600, 600);
             window.setScene(scene);
             window.show();
 
